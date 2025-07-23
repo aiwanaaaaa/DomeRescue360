26b6bad7ef20fa0df6330061f45ee6540447cf37
aiwanaaaaa <iwaboo_iwana24921@icloud.com>
2025-07-23 16:56:38 +0900

    レティクル制作

--- Assets/Script/Server/SensorFetvher.cs
+++ Assets/Script/Server/SensorFetvher.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Networking;
 
 public class SensorFetvher : MonoBehaviour
 {
-    public List<string> deviceIdList = new List<string>();
-
-    public SensorData data;
+    public List<string> deviceIdList = new List<string>();              //ユーザーIDを格納するリスト
+    public Dictionary<string, SensorData> sensorMap = new();            // 各デバイスのセンサーデータ
+    public UnityEvent<List<string>> OnDeviceIdsUpdated;
 
     void Start()
     {
@@ -15,22 +16,28 @@ public class SensorFetvher : MonoBehaviour
     }
     IEnumerator FetchDeviceIds()
     {
+        //URLに接続
         string url = $"http://localhost:3000/deviceIds";
         UnityWebRequest request = UnityWebRequest.Get(url);
         yield return request.SendWebRequest();
 
+        //URLに接続出来たか確認
         if (request.result == UnityWebRequest.Result.Success)
         {
+            //接続出来たらユーザーID取得からリストに格納
             string json = request.downloadHandler.text;
             deviceIdList = JsonArrayParsor.Parse(json);
+            OnDeviceIdsUpdated.Invoke(deviceIdList); // ← イベント発火！
             Debug.Log($"取得された Device ID 数: {deviceIdList.Count}");
             foreach (string id in deviceIdList)
             {
                 Debug.Log($" Device ID: {id}");
             }
 
+            //IDを使ってその他の値を取得しに行く
             StartCoroutine(PollSensorData(deviceIdList));
         }
+        //出来なかった場合エラーを出す
         else
         {
             Debug.LogError($"接続エラー: {request.error}");
@@ -39,16 +46,22 @@ public class SensorFetvher : MonoBehaviour
 
     IEnumerator PollSensorData(List<string> deviceId)
     {
+        //無限ループ
         while (true)
         {
             foreach (string id in deviceIdList)
             {
+                //URLに接続
                 UnityWebRequest req = UnityWebRequest.Get($"http://localhost:3000/sensor/latest/{id}");
                 yield return req.SendWebRequest();
 
+                //接続できるか確認
                 if (req.result == UnityWebRequest.Result.Success)
                 {
-                    data = JsonUtility.FromJson<SensorData>(req.downloadHandler.text);
+                    //接続出来たら値取得＋コンソールに出力
+                    SensorData data = JsonUtility.FromJson<SensorData>(req.downloadHandler.text);
+                    sensorMap[id] = data;
+
                     Debug.Log($"受信 - α:{data.alpha} β:{data.beta} γ:{data.gamma} x:{data.accel_x} y:{data.accel_y} z:{data.accel_z}");
                 }
                 else
@@ -57,7 +70,8 @@ public class SensorFetvher : MonoBehaviour
                 }
             }
 
-            yield return new WaitForSeconds(0.2f); // ← 200msごとに更新
+            //毎フレーム更新すると死ぬので200msごとに更新（ゲームの動きを見て調整必須）
+           // yield return new WaitForSeconds(0.05f); // ← 50msごとに更新
         }
     }
 }
