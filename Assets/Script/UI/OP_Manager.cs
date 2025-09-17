fa260a5b498217233c0e7648f8157712fe473fd5
aiwanaaaaa <iwaboo_iwana24921@icloud.com>
2025-09-17 15:10:52 +0900

    op

---
+++ Assets/Script/UI/OP_Manager.cs
@@ -0,0 +1,32 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OP_Manager : MonoBehaviour
+{
+    [SerializeField] private GameObject qr_code;
+    [SerializeField] private GameObject feed;
+    [SerializeField] private float qr_time;
+    [SerializeField] private float recture_time;
+    private float time = 0.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        Debug.Log(time);
+
+        if (time >= qr_time)
+            qr_code.SetActive(false);
+
+        if (time >= recture_time)
+ 
+            feed.gameObject.transform.localScale += new Vector3(1,1,0);
+    }
+}
